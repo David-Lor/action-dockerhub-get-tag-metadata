f74f7c989e9919d0f9ecfadbe77ece4118cf992b
David-Lor <17401854+David-Lor@users.noreply.github.com>
2021-12-07 12:34:37 +0100

    feat: initial release

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Github Action: DockerHub Fetch Image-Tag Metadata
 
-Github Action for fetching the information of a certain Docker image and tag (including the SHA digests), from the DockerHub public registry.
+Github Action for fetching the metadata of a certain Docker image and tag (including the SHA digests), from the DockerHub public registry.
+
+The API endpoint used for fetching the metadata is (with the example of the official Python image): `https://registry.hub.docker.com/v2/repositories/library/python/tags?page=1`
+
+## Action I/O
+
+### Inputs
+
+- `image`: full image name to find, with format `author/image:tag`, or `image:tag` for official images (required)
+- `os`: image OS to find (default: `linux`)
+- `architecture`: image architecture to find (default: `amd64`)
+- `pageLimit`: how many pages of results to parse, until giving up (default: `50`)
+
+### Outputs
+
+**If the image is not found, the Action will fail!**
+
+- `digest`: found image digest (example: `sha256:ec698176504f2f2d0e50e7e627d7db17be0c8c1a36fe34bb5b7c90c79355f7bb`)
+- `size`: found image size, in bytes (example: `43640637`)
+- Full JSON outputs from the API: these are categorized in two outputs:
+    - `tagMetadata` is the whole object for a certain tag (e.g. `python:slim-buster`). This includes an array of images, being each image a variant for a certain os and architecture
+    - `finalImageMetadata` is the whole object for a concrete image of the found tag, matching the input os and architecture. This object is filtered out from the array of images found on the "tagMetadata" object
---
+++ src/index.js
@@ -0,0 +1,153 @@
+const core = require('@actions/core');
+const got = require('got');
+
+const DEFAULT_OS = "linux";
+const DEFAULT_ARCH = "amd64";
+const DEFAULT_PAGE_LIMIT = 50;
+
+/**
+ * Get an Action input variable
+ * @param name key name of the variable
+ * @param defaultValue default value, if variable value is empty. If null, throw error in this case
+ */
+function getInputVariable(name, defaultValue) {
+    let value = core.getInput(name) || '';
+    value = value.trim();
+
+    if (!value) {
+        value = defaultValue;
+    }
+    if (value === null) {
+        throw new Error(`Input variable '${name}' not specified!`);
+    }
+
+    return value;
+}
+
+function parseImage(imageInput) {
+    if (!imageInput) {
+        throw new Error("No image specified");
+    }
+
+    let imageChunks = imageInput.split("/");
+    let author;
+    let image;
+    let tag = 'latest';
+
+    if (imageChunks.length == 1) {
+        author = 'library';
+        image = imageChunks[0];
+    } else if (imageChunks.length == 2) {
+        [author, image] = imageChunks;
+    } else {
+        throw new Error("Invalid image format");
+    }
+
+    imageChunks = image.split(":");
+    if (imageChunks.length == 2) {
+        [image, tag] = imageChunks;
+    }
+
+    return [author, image, tag];
+}
+
+/**
+ * Request tags metadata for a certain image
+ */
+async function request(author, image, page) {
+    const url = `https://registry.hub.docker.com/v2/repositories/${author}/${image}/tags?page=${page}`;
+    console.log(`Requesting ${url} ...`);
+    const r = await got(url);
+
+    const statusCode = r.statusCode;
+    const body = r.body;
+    console.log(`Response statuscode=${statusCode}`);
+    console.log(`Response body:\n${body}`);
+    if (statusCode !== 200) {
+        throw new Error(`Bad statuscode (got ${statusCode}, expected 200)`);
+    }
+
+    return body;
+}
+
+/**
+ * Parse a Response Body
+ * @returns Object with the found target image metadata
+ * @returns Null if target image not found in current page, but more pages are available
+ * @returns False if target image not found, and no more pages are available
+ */
+function parseResponse(responseBody, tag, os, architecture) {
+    const js = JSON.parse(responseBody);
+
+    for (let tagJs of js.results || []) {
+        if (tagJs.name !== tag) {
+            continue;
+        }
+
+        for (let imageJs of tagJs.images) {
+            let imageArch = imageJs.architecture;
+            let imageArchVariant = imageJs.variant;
+            if (imageArchVariant) {
+                imageArch = `${imageArch}/${imageArchVariant}`
+            }
+
+            if (imageArch !== architecture || imageJs.os !== os) {
+                continue;
+            }
+
+            // Target image found!
+            return {
+                digest: imageJs.digest,
+                size: imageJs.size,
+                tagMetadata: tagJs,
+                finalImageMetadata: imageJs
+            };
+        }
+    }
+
+    if (js.next && typeof js.next === 'string') {
+        return null;
+    }
+    return false;
+}
+
+async function fetchImageMetadata(author, image, tag, os, architecture, pageLimit) {
+    let page = 1;
+    let result = null;
+
+    while (result === null && page <= pageLimit) {
+        const responseBody = await request(author, image, page);
+        result = parseResponse(responseBody, tag, os, architecture);
+        page++;
+    }
+
+    if (typeof result !== 'object') {
+        throw new Error('Image-Tag not found!');
+    }
+
+    return result;
+}
+
+async function main() {
+    try {
+        const inputImage = getInputVariable('image', null);
+        const imageOS = getInputVariable('os', DEFAULT_OS);
+        const imageArch = getInputVariable('architecture', DEFAULT_ARCH);
+        const [imageAuthor, imageName, imageTag] = parseImage(inputImage);
+
+        let pageLimit = parseInt(getInputVariable('pageLimit', ''));
+        if (isNaN(pageLimit)) pageLimit = DEFAULT_PAGE_LIMIT;
+
+        console.log(`Target image: author=${imageAuthor} name=${imageName} tag=${imageTag} os=${imageOS} arch=${imageArch} pageLimit=${pageLimit}`);
+        const tagMetadata = await fetchImageMetadata(imageAuthor, imageName, imageTag, imageOS, imageArch, pageLimit);
+
+        core.setOutput('digest', tagMetadata.digest);
+        core.setOutput('size', tagMetadata.size);
+        core.setOutput('tagMetadata', tagMetadata.tagMetadata);
+        core.setOutput('finalImageMetadata', tagMetadata.finalImageMetadata);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+main();
